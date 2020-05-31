32b316c1bbccaef21acfdf913bb65d068fdec71d
usmanaliyu <osman.abbati@gmail>
2020-05-31 03:21:27 +0200

    first

---
+++ core/admin.py
@@ -0,0 +1,87 @@
+from django.contrib import admin
+
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile, Category
+
+
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+
+
+make_refund_accepted.short_description = 'Update orders to refund granted'
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'order_id',
+                    'ordered',
+                    'being_delivered',
+                    'received',
+                    'refund_requested',
+                    'refund_granted',
+                    'shipping_address',
+                    'billing_address',
+                    'payment',
+                    'coupon',
+                    
+                    ]
+    list_display_links = [
+        'user',
+        'shipping_address',
+        'billing_address',
+        'payment',
+        'coupon'
+    ]
+    list_filter = ['ordered',
+                   'being_delivered',
+                   'received',
+                   'refund_requested',
+                   'refund_granted']
+    search_fields = [
+        'user__username',
+        'ref_code'
+    ]
+    actions = [make_refund_accepted]
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'street_address',
+        'apartment_address',
+        'country',
+        'zip',
+        'address_type',
+        'default'
+    ]
+    list_filter = ['default', 'address_type', 'country']
+    search_fields = ['user', 'street_address', 'apartment_address', 'zip']
+
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'order_id',
+        'quantity',
+        'item',
+
+        
+    ]
+
+admin.site.register(Item,ItemAdmin)
+admin.site.register(OrderItem,OrderItemAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Payment)
+admin.site.register(Coupon)
+admin.site.register(Refund)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(UserProfile)
+admin.site.register(Category,CategoryAdmin)
+
+admin.site.site_header = "Jane's Admin"
+admin.site.site_text = "Jane's Admin Portal"
+admin.site.site_title = "Welcome to janes Admin Portal"
